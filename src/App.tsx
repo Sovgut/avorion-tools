b9668f9c56ec9a4a021785aa45468bd3c6afb56c
Sergey Sovgut <sergey.sovgut@outlook.com>
2023-07-30 18:17:20 +0300

    Added video background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import {Container} from "@mui/joy";
+import {Box, Container, useColorScheme} from "@mui/joy";
 import {TurretList} from "./components/turret/list";
 
 function App() {
+    const {mode} = useColorScheme();
+
     return (
-        <Container maxWidth={false}>
-            <TurretList/>
-        </Container>
+        <Box sx={{position: "relative", minHeight: "100vh"}}>
+            <Box sx={{
+                top: 0,
+                left: 0,
+                position: "fixed",
+                zIndex: "-2",
+                height: "100%",
+                width: "100%",
+                overflow: "hidden"
+            }}>
+                {mode === "dark" || mode === "system"
+                    ? <video style={{width: "100%", height: "100%", objectFit: "cover"}}
+                             src="/assets/video/background.mp4"
+                             autoPlay loop muted
+                             controls={false}/>
+                    : null}
+
+            </Box>
+            <Box sx={{
+                top: 0,
+                left: 0,
+                position: "fixed",
+                zIndex: "-1",
+                height: "100%",
+                width: "100%",
+                background: "linear-gradient(to bottom, transparent, var(--joy-palette-background-level1))",
+                opacity: ".75"
+            }}/>
+            <Container maxWidth={false}>
+                <TurretList/>
+            </Container>
+        </Box>
     );
 }
 
