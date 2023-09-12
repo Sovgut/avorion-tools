4b1b9a7f9fe4135965d3f466a357da654661dfea
Sergey Sovgut <sergey.sovgut@outlook.com>
2023-09-12 20:26:28 +0300

    Remove viewport height dependency from VideoBackground component
    The viewport height dependency was removed from the VideoBackground component to improve stability and performance. In the previous version, the video background height was being computed using the vh unit from the viewport height hook, which caused frequent re-rendering and unnecessary layout shifts on changes to viewport height. Now, the height of the video background is set to 100% to cover the whole viewport irrespective of its height. Also, the position of the container is changed from 'absolute' to 'fixed' for a similar reason, preventing layout shifts when scrolling.

--- src/components/video-background/index.tsx
+++ src/components/video-background/index.tsx
@@ -2,33 +2,31 @@ import {Box} from "@mui/joy";
 import React from "react";
 import {useTheme} from "@mui/joy/styles";
 import {useMediaQuery} from "react-responsive";
-import useVH from 'react-viewport-height';
+//import useVH from 'react-viewport-height';
 import styles from './styles.module.css';
 
 export function VideoBackground() {
-    const vh = useVH();
+    //const vh = useVH();
 
     const theme = useTheme();
     const isSmallScreen = useMediaQuery({query: `(max-width: ${theme.breakpoints.values.md}px)`});
 
     return (
-        <Box style={{height: `${100 * vh}px`, width: "100%"}} sx={{display: "flex", position: "fixed", zIndex: "-1"}}>
-            <Box className={styles.container}>
-                {!isSmallScreen && <video
-                    style={{
-                        position: "absolute",
-                        width: "100%",
-                        height: "100%",
-                        top: "0",
-                        left: "0",
-                        objectFit: "cover",
-                    }}
-                    src={`/assets/video/dark-background.mp4`}
-                    autoPlay
-                    loop
-                    muted
-                    controls={false}/>}
-            </Box>
+        <Box className={styles.container}>
+            {!isSmallScreen && <video
+                style={{
+                    position: "absolute",
+                    width: "100%",
+                    height: "100%",
+                    top: "0",
+                    left: "0",
+                    objectFit: "cover",
+                }}
+                src={`/assets/video/dark-background.mp4`}
+                autoPlay
+                loop
+                muted
+                controls={false}/>}
         </Box>
     )
 }
