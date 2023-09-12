278fe9364695c72dcf33acfbaed5dc179678450a
Sergey Sovgut <sergey.sovgut@outlook.com>
2023-09-12 20:26:57 +0300

    Remove unused viewport height hook from VideoBackground
    The useVH hook was imported and commented out in the VideoBackground component, but never actually utilised. This commit removes these comments, cleaning up the component and reducing potential confusion in the future.

--- src/components/video-background/index.tsx
+++ src/components/video-background/index.tsx
@@ -2,12 +2,9 @@ import {Box} from "@mui/joy";
 import React from "react";
 import {useTheme} from "@mui/joy/styles";
 import {useMediaQuery} from "react-responsive";
-//import useVH from 'react-viewport-height';
 import styles from './styles.module.css';
 
 export function VideoBackground() {
-    //const vh = useVH();
-
     const theme = useTheme();
     const isSmallScreen = useMediaQuery({query: `(max-width: ${theme.breakpoints.values.md}px)`});
 
