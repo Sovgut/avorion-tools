0fd250d2a8df0b2e2f09feb7722be35fe4f3bb0b
Sergey Sovgut <sergey.sovgut@outlook.com>
2023-09-15 13:06:13 +0300

    "Implement dynamic image transformation and animation enhancements"
    Modified ComponentIcon and TurretIcon components to include an image transformation function that replaces black pixels to transparent, improving those components' appearance. Also, removed redundant CSS mix-blend-mode properties. Added animation enhancements to components-table using framer-motion for smooth transitions. The changes aim to improve UI/UX by delivering better visuals and smoother animations.

--- src/components/components-table/index.tsx
+++ src/components/components-table/index.tsx
@@ -11,10 +11,10 @@ import {clearTurrets} from "~reducers/turret.ts";
 import {clearComponents} from "~reducers/component.ts";
 import {clearCargoComponents} from "~reducers/cargo.ts";
 import {clearComponentsCheckbox} from "~reducers/checkbox.ts";
-import styles from './styles.module.css';
 import {useTheme} from "@mui/joy/styles";
 import {useMediaQuery} from "react-responsive";
 import {computeComponents, initialComputationComponents} from "~utils/computations/price.ts";
+import {AnimatePresence, motion} from "framer-motion";
 
 export function ComponentsTable() {
     const [components, setComponents] = useState<Record<ComponentType, number>>({} as Record<ComponentType, number>)
@@ -56,44 +56,73 @@ export function ComponentsTable() {
     }
 
     return (
-        <Card className={styles.animation} variant="outlined" sx={{p: 0, gap: 0, boxShadow: "sm"}}>
+        <Card variant="outlined" sx={{p: 0, gap: 0, boxShadow: "sm"}}>
             <Typography level="body-lg" fontWeight="bold" textColor="white" sx={{p: 2, pb: 1, pt: 3}}>
                 {intlContext.text("UI", "cargo-required")}
             </Typography>
             <Divider sx={{mt: 2}}/>
-            <Table>
-                <tbody>
-                {(Object.keys(components) as ComponentType[]).sort((a, b) => a.localeCompare(b)).map(type => (
-                    <tr key={type}>
-                        <ComponentItemType type={type}/>
-                        <ComponentItemQuantity type={type} value={components[type]}/>
-                    </tr>
-                ))}
-                </tbody>
-            </Table>
-            {(computations.avg > 0 || computations.volume > 0) && (
-                <CardOverflow variant="soft" color="neutral" sx={{borderRadius: 0}}>
-                    <Divider inset="context" sx={{mt: 0}}/>
-                    <Stack>
-                        <Stack direction="row" justifyContent="space-between" sx={{p: 2, pt: 1, pb: 1}}>
-                            <Typography fontSize={fontSize}
-                                        level="body-sm">{intlContext.text("UI", "estimated-price")}</Typography>
-                            <Stack direction="row">
-                                <Typography fontSize={fontSize} level="body-sm">~</Typography>
-                                <Typography fontSize={fontSize}
-                                            level="body-sm">{computations.avg.toLocaleString()}</Typography>
+            <AnimatePresence>
+                <motion.div
+                    style={{overflow: "hidden"}}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                    layoutId="components-table"
+                    layout
+                >
+                    <Table>
+                        <tbody>
+                        <AnimatePresence>
+                            {(Object.keys(components) as ComponentType[]).sort((a, b) => a.localeCompare(b)).map(type => (
+                                <motion.tr
+                                    key={type}
+                                    initial={{opacity: 0}}
+                                    animate={{opacity: 1}}
+                                    exit={{opacity: 0}}
+                                    layoutId={`component-row-${type}`}
+                                    layout
+                                >
+                                    <ComponentItemType type={type}/>
+                                    <ComponentItemQuantity type={type} value={components[type]}/>
+                                </motion.tr>
+                            ))}
+                        </AnimatePresence>
+                        </tbody>
+                    </Table>
+                </motion.div>
+            </AnimatePresence>
+            <AnimatePresence>
+                {(computations.avg > 0 || computations.volume > 0) && (
+                    <motion.div
+                        initial={{opacity: 0, height: 0}}
+                        animate={{opacity: 1, height: "auto"}}
+                        exit={{opacity: 0, height: 0}}
+                        layout
+                    >
+                        <CardOverflow variant="soft" color="neutral" sx={{borderRadius: 0}}>
+                            <Divider inset="context" sx={{mt: 0}}/>
+                            <Stack sx={{width: "100%"}}>
+                                <Stack direction="row" justifyContent="space-between" sx={{p: 2, pt: 1, pb: 1}}>
+                                    <Typography fontSize={fontSize}
+                                                level="body-sm">{intlContext.text("UI", "estimated-price")}</Typography>
+                                    <Stack direction="row">
+                                        <Typography fontSize={fontSize} level="body-sm">~</Typography>
+                                        <Typography fontSize={fontSize}
+                                                    level="body-sm">{computations.avg.toLocaleString()}</Typography>
+                                    </Stack>
+                                </Stack>
+                                <Divider/>
+                                <Stack direction="row" justifyContent="space-between" sx={{p: 2, pt: 1, pb: 1}}>
+                                    <Typography fontSize={fontSize}
+                                                level="body-sm">{intlContext.text("UI", "estimated-volume")}</Typography>
+                                    <Typography fontSize={fontSize}
+                                                level="body-sm">{computations.volume.toLocaleString()}</Typography>
+                                </Stack>
                             </Stack>
-                        </Stack>
-                        <Divider/>
-                        <Stack direction="row" justifyContent="space-between" sx={{p: 2, pt: 1, pb: 1}}>
-                            <Typography fontSize={fontSize}
-                                        level="body-sm">{intlContext.text("UI", "estimated-volume")}</Typography>
-                            <Typography fontSize={fontSize}
-                                        level="body-sm">{computations.volume.toLocaleString()}</Typography>
-                        </Stack>
-                    </Stack>
-                </CardOverflow>
-            )}
+                        </CardOverflow>
+                    </motion.div>
+                )}
+            </AnimatePresence>
 
             <Divider/>
             <Box sx={{p: 2}}>
--- src/components/turret-icon/index.tsx
+++ src/components/turret-icon/index.tsx
@@ -1,17 +1,26 @@
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import {TurretsMeta} from "~constants/meta/turrets";
 import {TurretType} from "~constants/enums/turrets";
 import {IntlContext} from "~contexts/intl";
 import styles from "./styles.module.css";
+import {replaceBlackToTransparent} from "~utils/transformations/replace-black-to-transparent.ts";
 
 type Props = {
     type: TurretType;
 }
 
 export function TurretIcon({type}: Props) {
+    const [imageSrc, setImageSrc] = useState<string | null>(null);
     const intlContext = useContext(IntlContext);
 
-    return (
-        <img className={styles.icon} src={TurretsMeta[type].icon} alt={intlContext.text("TURRET", type)}/>
-    )
+    useEffect(() => {
+        (async () => {
+            const modifiedImageSrc = await replaceBlackToTransparent(TurretsMeta[type].icon);
+            setImageSrc(modifiedImageSrc);
+        })();
+    }, [type]);
+
+    return imageSrc ? (
+        <img className={styles.icon} src={imageSrc} alt={intlContext.text("TURRET", type)}/>
+    ) : null;
 }
