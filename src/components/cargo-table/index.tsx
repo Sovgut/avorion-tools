b7142ac44891cdb619f3322b6d567d65ee7a9ce6
Sergey Sovgut <sergey.sovgut@outlook.com>
2023-09-15 14:20:03 +0300

    Add animation to CargoTable component and expose dev command
    Added animation to the CargoTable component to provide a smoother transition when items are added or removed. This change was needed to enhance the user experience.
    
    In the package.json file, a "dev:expose" script was added to make the app accessible to network during the development. This was implemented for easier debugging and testing from different devices on the same network.

--- src/components/cargo-table/index.tsx
+++ src/components/cargo-table/index.tsx
@@ -4,10 +4,11 @@ import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "~store";
 import {Card, Divider, IconButton, Stack, Table, Typography} from "@mui/joy";
 import {ComponentType} from "~constants/enums/components";
-import {CargoItemType} from "~components/cargo-table/cargo-type";
-import {CargoItemQuantity} from "~components/cargo-table/cargo-quantity";
 import {ClearAll} from "@mui/icons-material";
 import {clearCargoComponents} from "~reducers/cargo.ts";
+import {AnimatePresence, motion} from "framer-motion";
+import {CargoItemType} from "~components/cargo-table/cargo-type";
+import {CargoItemQuantity} from "~components/cargo-table/cargo-quantity";
 
 export function CargoTable() {
     const intlContext = useContext(IntlContext);
@@ -23,26 +24,44 @@ export function CargoTable() {
     }
 
     return (
-        <Card variant="outlined" sx={{p: 0, gap: 0, boxShadow: "sm"}}>
-            <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={1} sx={{p: 2, pb: 0}}>
-                <Typography level="body-lg" fontWeight="bold" textColor="white">
-                    {intlContext.text("UI", "cargo")}
-                </Typography>
-                <IconButton onClick={handleResetCargo} variant="soft" color="danger">
-                    <ClearAll/>
-                </IconButton>
-            </Stack>
-            <Divider sx={{mt: 2}}/>
-            <Table>
-                <tbody>
-                {(Object.keys(cargo.entities) as ComponentType[]).sort((a, b) => a.localeCompare(b)).map(type => (
-                    <tr key={type}>
-                        <CargoItemType type={type}/>
-                        <CargoItemQuantity type={type} value={cargo.entities[type]}/>
-                    </tr>
-                ))}
-                </tbody>
-            </Table>
-        </Card>
+        <AnimatePresence>
+            <motion.div
+                initial={{opacity: 0, scale: .75}}
+                animate={{opacity: 1, scale: 1}}
+                layout
+            >
+                <Card variant="outlined" sx={{p: 0, gap: 0, boxShadow: "sm"}}>
+                    <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={1}
+                           sx={{p: 2, pb: 0}}>
+                        <Typography level="body-lg" fontWeight="bold" textColor="white">
+                            {intlContext.text("UI", "cargo")}
+                        </Typography>
+                        <IconButton onClick={handleResetCargo} variant="soft" color="danger">
+                            <ClearAll/>
+                        </IconButton>
+                    </Stack>
+                    <Divider sx={{mt: 2}}/>
+                    <Table>
+                        <tbody>
+                        <AnimatePresence>
+                            {(Object.keys(cargo.entities) as ComponentType[]).sort((a, b) => a.localeCompare(b)).map(type => (
+                                <motion.tr
+                                    key={type}
+                                    initial={{opacity: 0}}
+                                    animate={{opacity: 1}}
+                                    exit={{opacity: 0}}
+                                    layoutId={`cargo-row-${type}`}
+                                    layout
+                                >
+                                    <CargoItemType type={type}/>
+                                    <CargoItemQuantity type={type} value={cargo.entities[type]}/>
+                                </motion.tr>
+                            ))}
+                        </AnimatePresence>
+                        </tbody>
+                    </Table>
+                </Card>
+            </motion.div>
+        </AnimatePresence>
     )
 }
